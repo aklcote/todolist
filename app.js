05872fc67efbc900ec52fc27438f11d934fea68a
aklcote <aklcote@gmail.com>
2023-04-03 12:25:19 -0700

    add full driver code

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ app.use(express.static("public"));
 mongoose.connect("mongodb+srv://aklcote:QItFi1hl4BsNgP2k@cluster0.ukzj5wl.mongodb.net/todolistDB")
 mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://aklcote:<password>@cluster0.ukzj5wl.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 const itemsSchema = {
   name: String
 }
